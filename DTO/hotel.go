9e9b0dc4b299ef3916ef64bd699aec2611e5353c
lucilaGomez <luligomez.ar@gmail.com>
2023-10-23 10:14:50 -0300

    n

--- DTO/hotel.go
+++ DTO/hotel.go
@@ -6,6 +6,7 @@ type HotelDTO struct {
 	Description string  `json:"description" validate:"required"`
 	Price       float64 `json:"price" validate:"required"`
 	Rooms       int64   `json:"rooms" validate:"required"`
+	Images       ImagesDto `json:"images,omitempty"`
 }
 
 type HotelsDTO []HotelDTO
